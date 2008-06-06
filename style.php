fc269f30cb29d18931fa3428cf34ece58c12e877
ngervais <ngervais>
2008-06-06 20:56:03 +0000

    *** empty log message ***

---
+++ style.php
@@ -0,0 +1,38 @@
+<?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();	   # All on the same line to unclutter the user's desktop'
+
+	#*****************************************************************************
+	#
+	# index.php
+	#
+	# Author: 	 	Nathan Gervais
+	# Date:			2008-05-16
+	#
+	# Description: Ganymede Spotting Submission Form
+	#
+	#****************************************************************************
+	
+	# Add page-specific Nav bars here
+	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank)
+	# $Nav->addCustomNav("My Link", "mypage.php", "_self");
+	# $Nav->addCustomNav("Google", "http://www.google.com/", "_blank");
+	$pageTitle 		= "Ganymede Around the World";
+	$pageKeywords	= "eclipse ganymede, ganymede, ganymede around the world";
+	$pageAuthor		= "Nathan Gervais";
+	# End: page-specific settings
+	#
+
+	extract($_GET);
+	
+	# Place your html content in a file called content/en_pagename.php
+	require_once("/home/data/httpd/eclipse-php-classes/system/dbconnection_rw.class.php");
+	$dbc = new DBConnectionRW();
+	$dbh = $dbc->connect();
+
+	if (isset($id))
+	{
+		$query = "DELETE FROM ganymede_spots WHERE id = $id";
+		echo $query;
+	}
+	# Generate the web page
+	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
+?>	
