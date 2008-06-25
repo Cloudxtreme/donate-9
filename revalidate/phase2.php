ac25521e7106aeecc9638430729ebe706f08c12a
ngervais <ngervais>
2008-06-25 04:28:39 +0000

    *** empty log message ***

--- revalidate/phase2.php
+++ revalidate/phase2.php
@@ -39,7 +39,8 @@
 	{
 		$friend->setBugzillaID($bugzillaID);
 		$SQL = "UPDATE friends SET bugzilla_id = $bugzillaID WHERE friend_id = ". $friend->getFriendID();
-		$result = mysql_query($SQL) or die(mysql_error());
+		echo $SQL;
+		$result = mysql_query($SQL,$dbh) or die(mysql_error());
 		
 		echo "Friend Updated";
 	}
