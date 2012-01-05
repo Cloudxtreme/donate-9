49ce4a3103e4ef4c0e129166beb8a4461f6c7d29
droy <droy>
2012-01-05 13:59:02 +0000

    *** empty log message ***

--- donorlist.php
+++ donorlist.php
@@ -1,5 +1,8 @@
 <?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
 
+
+error_reporting(E_ALL); 
+	ini_set("display_errors", true);
 	#*****************************************************************************
 	#
 	# donorlist.php (/friends)
