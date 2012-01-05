b8bb76cfc74d8d4143c4dc9e0a0d6216b61edf40
droy <droy>
2012-01-05 15:38:47 +0000

    Fix adding one month

--- donorlist.php
+++ donorlist.php
@@ -103,9 +103,6 @@
 						}
 					$date = strtotime("-1 year", strtotime($contribution->getDateExpired()));
 					$now = strtotime("now");
-					if ($date > $now) {
-						$date = strtotime("-13 month", strtotime($contribution->getDateExpired()));
-					}
 					if ($date <= $now) {
 						$date = date("Y-m-d", $date);
 						//$date = $friend->getDateJoined();		
